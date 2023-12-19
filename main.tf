9887b0c1cded51f11a3e99819e1dc3b7867ef784
Harry Dam <haidv.ict@gmail.com>
2023-12-19 12:26:15 +0700

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,40 @@
+provider "aws" {
+  region = "us-west-2"  # Ví dụ, bạn có thể thay đổi theo region của bạn
+}
+
+resource "aws_cloudfront_distribution" "eks_distribution" {
+  for_each = var.eks_clusters
+
+  origin {
+    domain_name = each.value.domain_name
+    origin_id   = each.key
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = each.key
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "whitelist"
+      locations        = each.value.locations
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
